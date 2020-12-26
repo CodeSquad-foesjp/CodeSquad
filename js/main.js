6772b89a5bba9d88f70f800a5b4459c6bbc4bbeb
CodeSquad-foesjp <76586717+CodeSquad-foesjp@users.noreply.github.com>
2020-12-26 23:35:32 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -150,7 +150,7 @@
 		var timeinterval = setInterval(updateClock, 1000);
 	}
 
-	var deadline = "january 16,2021 21:00:00"//new Date(Date.parse(new Date()) + 6 * 24 * 60 * 60 * 1000);
+	var deadline = "january 16,2021 20:00:00"//new Date(Date.parse(new Date()) + 6 * 24 * 60 * 60 * 1000);
 	initializeClock('clockdiv', deadline);
 
 	/*----------------------------------------------------*/
